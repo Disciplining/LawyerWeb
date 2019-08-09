b12accf4f71df31c4b829a2eb9ffb1eaaf65b5e3
liyanxing <416698547@qq.com>
2019-08-09 16:34:18 +0800

    项目创建

---
+++ src/main/java/com/hyg/LawyerWebApplication.java
@@ -0,0 +1,13 @@
+package com.hyg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LawyerWebApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LawyerWebApplication.class, args);
+	}
+}
