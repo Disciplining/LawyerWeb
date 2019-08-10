425e4729bc52838601b8665ca9f7f68354134547
wangzf <wangzf@121ugrow.com>
2019-08-10 17:29:06 +0800

    一级目录

--- src/main/java/com/hyg/service/MenuService.java
+++ src/main/java/com/hyg/service/MenuService.java
@@ -1,7 +1,22 @@
 package com.hyg.service;
 
+import com.hyg.mapper.MenuMapper;
+import com.hyg.pojo.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service("menuService")
 public class MenuService {
+
+    @Autowired
+    @Qualifier("menuMapper")
+    private MenuMapper menuMapper;
+
+    public List<Menu> findTopMenu(){
+        return menuMapper.findTopMenu();
+    }
+
 }
