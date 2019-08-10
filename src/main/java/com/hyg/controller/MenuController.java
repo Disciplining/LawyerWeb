fadc50a9f9df8ba330cebce985070c56eaadc4f2
wangzf <wangzf@121ugrow.com>
2019-08-10 17:28:11 +0800

    一级目录

--- src/main/java/com/hyg/controller/MenuController.java
+++ src/main/java/com/hyg/controller/MenuController.java
@@ -1,18 +1,28 @@
 package com.hyg.controller;
 
+import com.hyg.pojo.Menu;
+import com.hyg.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * 菜单控制器
  */
 @Controller
 public class MenuController {
+    @Autowired
+    @Qualifier("menuService")
+    public MenuService menuService;
+
     @ResponseBody
     @RequestMapping("/firstMenu")
-    public String  findMenu(){
-        return "hello";
+    public List<Menu> findMenu(){
+        return menuService.findTopMenu();
     }
 
 }
