36df0977595f7006db5dd68759edbc37e3e93b1d
wangzf <wangzf@121ugrow.com>
2019-08-13 09:18:27 +0800

    一级目录

---
+++ src/main/java/com/hyg/service/LawyerService.java
@@ -0,0 +1,24 @@
+package com.hyg.service;
+
+import com.hyg.mapper.LawyerMapper;
+import com.hyg.pojo.Lawyer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * 律师
+ */
+@Service("lawyerService")
+public class LawyerService {
+    @Qualifier("lawyerMapper")
+    @Autowired
+    private LawyerMapper lawyerMapper;
+
+    public List<Lawyer> findAllLawyer(){
+        return lawyerMapper.findAllLawyer();
+    }
+
+}
