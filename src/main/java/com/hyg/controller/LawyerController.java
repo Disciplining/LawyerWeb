be9c02ff85339122322bd755e425a525d1bb2a97
wangzf <wangzf@121ugrow.com>
2019-08-13 09:17:44 +0800

    一级目录

---
+++ src/main/java/com/hyg/controller/LawyerController.java
@@ -0,0 +1,24 @@
+package com.hyg.controller;
+
+import com.hyg.pojo.Lawyer;
+import com.hyg.service.LawyerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class LawyerController {
+    @Qualifier("lawyerService")
+    @Autowired
+    private LawyerService lawyerService;
+    @ResponseBody
+    @RequestMapping("/lawyerList")
+    public List<Lawyer> findAllLawyer(){
+        return lawyerService.findAllLawyer();
+    }
+
+}
