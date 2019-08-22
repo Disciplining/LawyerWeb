b763b65bcb275c978b61a2f15b1f90547db8234c
liyanxing <416698547@qq.com>
2019-08-22 21:13:14 +0800

    图片拼接主机地址

--- src/main/java/com/hyg/controller/CarouselController.java
+++ src/main/java/com/hyg/controller/CarouselController.java
@@ -1,13 +1,11 @@
 package com.hyg.controller;
 
 import com.hyg.pojo.Carousel;
-import com.hyg.pojo.Menu;
 import com.hyg.service.CarouselService;
+import com.hyg.util.NetUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,23 +13,23 @@ import java.util.List;
 
 /**
  * 轮播图控制器
-**/
+ **/
 @Controller
-public class CarouselController {
+public class CarouselController
+{
+	@Autowired
+	@Qualifier("carouselService")
+	public CarouselService carouselService;
 
-    @Autowired
-    @Qualifier("carouselService")
-    public CarouselService carouselService;
-    @Value("${cbs.projectPath}")
-    public String projectPath;
-    @ResponseBody
-    @RequestMapping("/carousel")
-    public List<Carousel> findCarousel(){
-    	List<Carousel> list = carouselService.findCarousel();
-    	for(Carousel ca:list){
-    		ca.setImgPath(projectPath.concat(ca.getImgPath()));
+	@ResponseBody
+	@RequestMapping("/carousel")
+	public List<Carousel> findCarousel()
+	{
+		List<Carousel> list = carouselService.findCarousel();
+		for (Carousel ca : list)
+		{
+			ca.setImgPath( NetUtil.getPcHost().append(ca.getImgPath()).toString() );
 		}
-        return list;
-    }
-
+		return list;
+	}
 }
--- src/main/java/com/hyg/controller/LawyerController.java
+++ src/main/java/com/hyg/controller/LawyerController.java
@@ -2,9 +2,9 @@ package com.hyg.controller;
 
 import com.hyg.pojo.Lawyer;
 import com.hyg.service.LawyerService;
+import com.hyg.util.NetUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,21 +15,22 @@ import java.util.List;
  * 律师表控制器
  */
 @Controller
-public class LawyerController {
-    @Qualifier("lawyerService")
-    @Autowired
-    private LawyerService lawyerService;
-    @Value("${cbs.projectPath}")
-    public String projectPath;
-    @ResponseBody
-    @RequestMapping("/lawyerList")
-    public List<Lawyer> findAllLawyer()
-    {
-        List<Lawyer> list=lawyerService.findAllLawyer();
-        for (Lawyer la:list){
-            la.setLawyerImg(projectPath.concat(la.getLawyerImg()));
-        }
-        return list;
-    }
+public class LawyerController
+{
+	@Qualifier("lawyerService")
+	@Autowired
+	private LawyerService lawyerService;
 
+	@ResponseBody
+	@RequestMapping("/lawyerList")
+	public List<Lawyer> findAllLawyer()
+	{
+		List<Lawyer> list = lawyerService.findAllLawyer();
+		for (Lawyer la : list)
+		{
+			la.setLawyerImg( NetUtil.getPcHost().append(la.getLawyerImg()).toString() );
+		}
+
+		return list;
+	}
 }
