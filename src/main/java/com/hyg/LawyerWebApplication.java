98a704dc0543431e06c39f20e7122bcc1a0ead95
liyanxing <416698547@qq.com>
2019-08-22 16:28:53 +0800

    获得罪名详情

--- src/main/java/com/hyg/LawyerWebApplication.java
+++ src/main/java/com/hyg/LawyerWebApplication.java
@@ -4,6 +4,8 @@ package com.hyg;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -11,7 +13,7 @@ import org.springframework.web.filter.CorsFilter;
 
 @MapperScan("com.**.mapper")
 @SpringBootApplication
-public class LawyerWebApplication
+public class LawyerWebApplication extends SpringBootServletInitializer
 {
 	public static void main(String[] args)
 	{
@@ -33,5 +35,16 @@ public class LawyerWebApplication
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", buildConfig());
 		return new CorsFilter(source);
+	}
+
+	/**
+	 * 发布项目
+	 * @param builder
+	 * @return
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder)
+	{
+		return builder.sources(this.getClass());
 	}
 }
