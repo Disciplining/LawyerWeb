e8ad815116863283151e9c1082740362434afa6b
liyanxing <416698547@qq.com>
2019-08-26 08:31:38 +0800

    主机地址写死

--- src/main/java/com/hyg/util/NetUtil.java
+++ src/main/java/com/hyg/util/NetUtil.java
@@ -3,13 +3,6 @@ package com.hyg.util;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.SocketException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 @Component
 public class NetUtil
 {
@@ -17,11 +10,11 @@ public class NetUtil
 	private static String port;
 
 	// 主机的地址
-	// 没有虚拟机的情况下就是第一个
-	private static final String host = getAllIpAddr().get(0);
+	private static final String host = "39.96.175.127";
 
 	/**
 	 * 获得主机地址
+	 * 要接招到返回的图片上url上
 	 * http://xxxx:xx
 	 * 最后没有 "/"
 	 * @return
@@ -29,45 +22,6 @@ public class NetUtil
 	public static StringBuffer getPcHost()
 	{
 		return new StringBuffer("http://" + host + ":" + port);
-	}
-
-	/**
-	 * 获得主机上所有网卡的IP
-	 * 包括虚拟网卡
-	 * @return
-	 */
-	private static List<String> getAllIpAddr()
-	{
-		List<String> ipList = new ArrayList<String>();
-		try
-		{
-			for (NetworkInterface networkInterface : Collections.list(NetworkInterface.getNetworkInterfaces()))
-			{
-				if (networkInterface.isVirtual())
-					continue;
-				if (!networkInterface.isUp())
-					continue;
-				for (InetAddress addr : Collections.list(networkInterface.getInetAddresses()))
-				{
-					if (!addr.isLoopbackAddress() && !addr.isLinkLocalAddress()
-							&& addr.isSiteLocalAddress())
-					{
-						ipList.add(addr.getHostAddress().toString());
-					}
-				}
-			}
-
-			System.out.println(ipList);
-		}
-		catch (SocketException e)
-		{
-			System.out.println("出现异常：" + e.getLocalizedMessage());
-
-			ipList.clear();
-			ipList.add("");
-		}
-
-		return ipList;
 	}
 
 	/**
